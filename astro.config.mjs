fac80aab8c6ff2c904533a97c14da3ddc55170d5
Eliott Wantz <70651737+EliottWantz@users.noreply.github.com>
2024-04-07 21:16:02 -0400

    update site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://eliottwantz.github.io/portfolio',
+    site: 'https://develiott.com',
     integrations: [
         mdx(),
         sitemap(),
