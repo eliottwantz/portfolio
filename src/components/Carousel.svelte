d5b6178fd157a61492b69f6c017b7de3db50d931
Eliott Wantz <70651737+EliottWantz@users.noreply.github.com>
2024-04-07 23:35:25 -0400

    added nem project

--- src/components/Carousel.svelte
+++ src/components/Carousel.svelte
@@ -1,24 +1,48 @@
 <script lang="ts">
     import * as Carousel from '@/components/ui/carousel/index';
+    import type { CarouselAPI } from '@/components/ui/carousel/context';
     import * as Card from '@/components/ui/card/index';
 
-    // export let images: string[] = [];
+    export let images: { src: string; alt: string }[];
+
+    let api: CarouselAPI;
+    let current = 0;
+    let count = 0;
+
+    $: if (api) {
+        count = api.scrollSnapList().length;
+        current = api.selectedScrollSnap() + 1;
+
+        api.on('select', () => {
+            current = api.selectedScrollSnap() + 1;
+        });
+    }
 </script>
 
-<Carousel.Root class="w-full max-w-xs">
-    <Carousel.Content>
-        {#each Array(5) as _, i (i)}
-            <Carousel.Item>
-                <div class="p-1">
+<div class="flex flex-col gap-y-2 items-center">
+    <Carousel.Root
+        bind:api
+        opts={{
+            loop: true
+        }}
+        class="w-screen sm:max-w-screen-md"
+    >
+        <Carousel.Content>
+            {#each images as image, i (i)}
+                <Carousel.Item>
                     <Card.Root>
-                        <Card.Content class="flex aspect-square items-center justify-center p-6">
-                            <span class="text-4xl font-semibold">{i + 1}</span>
+                        <Card.Content class="overflow-hidden flex aspect-square items-center justify-center">
+                            <img src={image.src} alt={image.alt} />
                         </Card.Content>
+                        <small class="pl-6 text-sm text-muted-foreground">{image.alt}</small>
                     </Card.Root>
-                </div>
-            </Carousel.Item>
-        {/each}
-    </Carousel.Content>
-    <Carousel.Previous />
-    <Carousel.Next />
-</Carousel.Root>
+                </Carousel.Item>
+            {/each}
+        </Carousel.Content>
+        <Carousel.Previous />
+        <Carousel.Next />
+    </Carousel.Root>
+    <div class="pt-4 text-center text-sm text-muted-foreground">
+        Image {current} of {count}
+    </div>
+</div>
