6ebb1624cee0d083fd8c0f932b07de8eee48aa59
Eliott Wantz <70651737+EliottWantz@users.noreply.github.com>
2024-04-07 21:31:26 -0400

    added svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
-import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import svelte from '@astrojs/svelte';
 import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,6 +12,7 @@ export default defineConfig({
         sitemap(),
         tailwind({
             applyBaseStyles: false
-        })
+        }),
+        svelte()
     ]
 });
