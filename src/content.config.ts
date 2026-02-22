5db67b27a873b0ca3a328cf8e7d6cf5fa4d4071e
Eliott Wantz <eliott.wantz@develiott.com>
2026-02-22 15:40:04 -0500

    improve types & page transition

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,23 +3,23 @@ import { glob } from "astro/loaders";
 import { z } from "astro/zod";
 
 const projects = defineCollection({
-  loader: glob({ pattern: "**/*.mdx", base: "./src/content/projects" }),
-  schema: z.object({
-    title: z.string(),
-    slug: z.string(),
-    description: z.string(),
-    publishDate: z.coerce.date(),
-    isFeatured: z.boolean().default(false),
-    tags: z.array(z.string()).default([]),
-    seo: z
-      .object({
-        image: z.object({
-          src: z.string(),
-          alt: z.string(),
-        }),
-      })
-      .optional(),
-  }),
+	loader: glob({ pattern: "**/*.mdx", base: "./src/content/projects" }),
+	schema: z.object({
+		title: z.string(),
+		slug: z.string(),
+		description: z.string(),
+		publishDate: z.coerce.date(),
+		isFeatured: z.boolean().default(false),
+		tags: z.array(z.string()).default([]),
+		seo: z
+			.object({
+				image: z.object({
+					src: z.string(),
+					alt: z.string(),
+				}),
+			})
+			.optional(),
+	}),
 });
 
 export const collections = { projects };
