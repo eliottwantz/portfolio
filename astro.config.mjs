979aa9900b5c268175caed4c6f2f1dbfbce25f1e
Eliott Wantz <eliott.wantz@develiott.com>
2026-02-22 16:15:46 -0500

    imageService: "compile"

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,23 @@
 // @ts-check
 
+import cloudflare from "@astrojs/cloudflare";
 import mdx from "@astrojs/mdx";
 import svelte from "@astrojs/svelte";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
-import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
 export default defineConfig({
-  prefetch: {
-      prefetchAll: true,
+	prefetch: {
+		prefetchAll: true,
 	},
-
-  vite: {
-      plugins: [tailwindcss()],
+	vite: {
+		plugins: [tailwindcss()],
 	},
-
-  site: "https://develiott.com",
-  integrations: [mdx(), svelte()],
-  adapter: cloudflare(),
+	output: "static",
+	site: "https://develiott.com",
+	integrations: [mdx(), svelte()],
+	adapter: cloudflare({
+		imageService: "compile",
+	}),
 });
